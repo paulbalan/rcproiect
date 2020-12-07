94888895073086a3f202fcd128d6c36ba58e0c74
Batalan Vlad <batabtro@gmail.com>
2020-12-07 15:06:35 +0200

    subscribe/suback ClientMQTT baseplot

--- BibliotecaMqtt/mqtt_server_side.py
+++ BibliotecaMqtt/mqtt_server_side.py
@@ -1,8 +1,141 @@
 from mqtt_lib import *
 import socket
+import random
+import string
+import threading
+
 
 # Acest module foloseste socketuri pentru a facilita
 #   trimiterea si primirea de pachete client -> server
+class SenderReceiver:
+    def __init__(self, conn):
+        self.encoder = GenericPackageEncoder()
+        self.decoder = GenericPackageDecoder()
+        self.conn = conn
+        pass
+
+    def sendPackage(self, package):
+        # encode package to binary and send to conenction
+        text = self.encoder.encode(package)
+        sent_bytes = self.conn.send(str_to_binary(text))
+        return sent_bytes
+
+    def receivePackage(self) -> IControlPackage:
+        # receive Fixed Header and then the variable part
+        # create fixedHeader
+        header_content = ""
+
+        # read flags
+        flags = self.conn.recv(1)
+        header_content += binary_to_str(flags)
+
+        # read remaining length
+        remLength = binary_to_str(self.conn.recv(1))
+        header_content += remLength
+        while remLength[0] == '1':
+            remLength = binary_to_str(self.conn.recv(1))
+            header_content += remLength
+
+        # decode header
+        header_component = self.decoder.decodeFixedHeader(header_content)
+
+        # based on the rem length, read variable header
+        remLength = header_component.getRemainingLength()
+
+        variable_content = binary_to_str(self.conn.recv(remLength))
+        package = self.decoder.decodeVariableComponents(variable_content, header_component)
+
+        return package
+
+
+class ClientMQTT:
+    def __init__(self, addr):
+        self.isConnected = False
+        self.conn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.transmitter = SenderReceiver(self.conn)
+
+        self.packedId = 0
+
+        # generate unique client id
+        length = random.randint(4, 10)
+        letters_and_digits = string.ascii_letters + string.digits
+        result_str = ''.join((random.choice(letters_and_digits) for i in range(length)))
+
+        self.clientId = result_str
+
+        print("Client trying to get the Broker socket ...")
+        self.conn.connect(addr)
+        print("Broker socket aquired.")
+
+    def connect(self, flags, keep_alive, username='', password='', willTopic='', willMessage=''):
+
+        # create connect package
+        builder = ConnectBuilder()
+        builder.buildFixedHeader()
+        builder.buildVariableHeader(flags, keep_alive)
+        builder.buildPayload(self.clientId, username=username, password=password, willMessage=willMessage,
+                             willTopic=willTopic)
+        connectPackage = builder.getPackage()
+
+        print("Connecting as " + username + " ...")
+        # send a connect package
+        self.transmitter.sendPackage(connectPackage)
+
+        # receive connack
+        connackPackage = self.transmitter.receivePackage()
+
+        return_code = connackPackage.getVariableHeader().getField("connect_return_code")
+
+        if return_code == 0:
+            print("Conected successfully!")
+        else:
+            print("Connection failed! Return code = " + str(return_code))
+
+    def subscribe(self, topics, QoS):
+        if isinstance(topics, str):
+            topics = [topics]
+        if isinstance(QoS, str):
+            QoS = int(QoS)
+        if isinstance(QoS, int):
+            QoS = [QoS]
+
+        self.packedId += 1
+
+        builder = SubscribeBuilder()
+        builder.buildFixedHeader()
+        builder.buildVariableHeader(self.packedId)
+        builder.buildPayload(topics, QoS)
+        subscribePackage = builder.getPackage()
+
+        # trimiterea pachetului de subscribe
+        self.transmitter.sendPackage(subscribePackage)
+
+        # astept raspuns de subscribe
+        subackPackage = self.transmitter.receivePackage()
+
+        if subackPackage.getVariableHeader().getField("packet_id") != self.packedId:
+            print("Id Packets for suback does not match!")
+
+        for index in range(0, len(topics)):
+            return_code = subackPackage.getPayload().getField("return_code_" + str(index))
+            print("Trying to subscribe to " + topics[index] + "...")
+            if return_code == 0x80:
+                print("\tResult = FAILURE")
+            else:
+                print("\tResult = SUCCESS")
+                print("\tQos admitted = " + str(return_code))
+
+    def disconnect(self):
+        # create disconnect
+        builder = DisconnectBuilder()
+        builder.reset()
+        builder.buildFixedHeader()
+        builder.buildVariableHeader()
+        builder.buildPayload()
+        disconnectPackage = builder.getPackage()
+
+        self.transmitter.sendPackage(disconnectPackage)
+        print("Client disconnected!")
 
 
 if __name__ == "__main__":
@@ -10,93 +143,16 @@ if __name__ == "__main__":
     ip = socket.gethostbyname(socket.gethostname())
     port = 1883
     address = (ip, port)
-    print(address)
 
-    # connect to broker using tcp/ip
-    my_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    #my_socket.bind(address)
+    username = input("Username = ")
 
-    print("Before accepting")
-    my_socket.connect(address)
-    print("Accepted")
+    client = ClientMQTT(address)
+    client.connect("10000110", 0, username=username, willTopic="/register",
+                   willMessage="Hello everyone! I am " + username)
+    client.subscribe(["/result", "/porn"], [2, 1])
 
-    # create connect package
-    builder = ConnectBuilder()
-    builder.buildFixedHeader()
-    builder.buildVariableHeader("10000110", 0)
-    builder.buildPayload("firstClient", username="vladbatalan", willMessage="hello world!", willTopic="/register")
-    connectPk = builder.getPackage()
+    start_key = input("Start? PressKet = Any ...\n")
+    #client.receive_thread.start()
+    exit_key = input("Exit? PressKey = Any ...\n")
 
-    # create encoders and decoders
-    encoder = GenericPackageEncoder()
-    decoder = GenericPackageDecoder()
-
-    print("Before sending")
-    # send a connect package
-    content = encoder.encode(connectPk) # is a string containing 1 s and zeros
-    displayControlPackageBinary(content)
-
-    # print("encoded content = " + str(content.encode('UTF-8')))
-    sent = my_socket.send(str_to_binary(content))
-    print(str(sent) + " bytes were sent")
-
-
-
-    # receive a connack
-    print("starting recv ...")
-    raw = my_socket.recv(4)
-    print("raw received = " + str(raw))
-    buffer = binary_to_str(raw)
-    print("ended recv")
-    print("buffer = " + buffer)
-    fixedHeader = decoder.decodeFixedHeader(buffer[0:16])
-
-    if fixedHeader.getType() == 2:
-        connackPk = decoder.decodeVariableComponents(buffer[16:], fixedHeader)
-        print(str(connackPk))
-    else:
-        print(str(fixedHeader.getType()) + "received")
-
-    # ping request
-    builder = PingreqBuilder()
-    builder.reset()
-    builder.buildFixedHeader()
-    builder.buildVariableHeader()
-    builder.buildPayload()
-    pingReq = builder.getPackage()
-
-    print(str(pingReq))
-    my_socket.send(str_to_binary(encoder.encode(pingReq)))
-
-
-    # receive a pingResp
-    print("starting recv ...")
-    raw = my_socket.recv(2)
-    print("raw received = " + str(raw))
-    buffer = binary_to_str(raw)
-    print("ended recv")
-    print("buffer = " + buffer)
-    fixedHeader = decoder.decodeFixedHeader(buffer[0:16])
-
-    if fixedHeader.getType() == 2:
-        pingResp = decoder.decodeVariableComponents(buffer[16:], fixedHeader)
-        print(str(pingResp))
-    else:
-        print(str(fixedHeader.getType()) + " received")
-
-
-
-    # create disconnect
-    builder = DisconnectBuilder()
-    builder.reset()
-    builder.buildFixedHeader()
-    builder.buildVariableHeader()
-    builder.buildPayload()
-    disconnect = builder.getPackage()
-
-    disconnect_text = encoder.encode(disconnect)
-    my_socket.send(str_to_binary(disconnect_text))
-
-
-
-
+    client.disconnect()
